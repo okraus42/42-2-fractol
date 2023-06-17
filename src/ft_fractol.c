67e114202bcbf4725ad7e2ece7a81a7282342c67
Ondřej Kraus <okraus42+github@pm.me>
2023-06-17 12:44:40 +0200

    norminette

--- src/ft_fractol.c
+++ src/ft_fractol.c
@@ -6,7 +6,7 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:35:23 by okraus            #+#    #+#             */
-/*   Updated: 2023/06/17 12:23:58 by okraus           ###   ########.fr       */
+/*   Updated: 2023/06/17 12:44:03 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 #define WIDTH 512
 #define HEIGHT 512
 
-void zoom(double xdelta, double ydelta, void* param)
+void	zoom(double xdelta, double ydelta, void *param)
 {
-	max_t*	max;
+	max_t		*max;
 	long double	len;
 
 	max = param;
 	len = max->data->l;
 	mlx_get_mouse_pos(max->mlx, &(max->data->x), &(max->data->y));
-	if (ydelta < 0 && max->data->z > 1 && max->data->x >=0 && max->data->x <= 511
-		&& max->data->y >=0 && max->data->y <= 511)
+	if (ydelta < 0 && max->data->z > 1 && max->data->x >= 0 && max->data->x <= 511
+		&& max->data->y >= 0 && max->data->y <= 511)
 	{
 		max->data->z -= 1;
 		max->data->l *= 1.25;
 	}
-	if (ydelta > 0 && max->data->z < 256 && max->data->x >=0 && max->data->x <= 511
-		&& max->data->y >=0 && max->data->y <= 511)
+	if (ydelta > 0 && max->data->z < 256 && max->data->x >= 0 && max->data->x <= 511
+		&& max->data->y >= 0 && max->data->y <= 511)
 	{
 		max->data->z += 1;
 		max->data->l *= 0.8;
@@ -42,17 +42,18 @@ void zoom(double xdelta, double ydelta, void* param)
 }
 
 //Mandelbrot
-int ft_iter_1(max_t* max)
+int	ft_iter_1(max_t	*max)
 {
 	int			iter;
 	long double	zx;
 	long double	zy;
 	long double	cx;
 	long double	cy;
+
 	zx = 0;
 	zy = 0;
 	cx = 0;
-	cy = 0; 
+	cy = 0;
 	iter = 0;
 	while (iter < max->data->iter && (cx + cy) <= 4)
 	{
@@ -65,19 +66,19 @@ int ft_iter_1(max_t* max)
 	return (iter);
 }
 
-
 //Julia
-int ft_iter_2(max_t* max)
+int	ft_iter_2(max_t	*max)
 {
 	int			iter;
 	long double	zx;
 	long double	zy;
 	long double	cx;
 	long double	cy;
+
 	zx = 0;
 	zy = 0;
 	cx = 0;
-	cy = 0; 
+	cy = 0;
 	iter = 0;
 	while (iter < max->data->iter && (cx + cy) <= 4)
 	{
@@ -91,17 +92,18 @@ int ft_iter_2(max_t* max)
 }
 
 //Multibrot
-int ft_iter_3(max_t* max)
+int	ft_iter_3(max_t	*max)
 {
 	int			iter;
 	long double	zx;
 	long double	zy;
 	long double	cx;
 	long double	cy;
+
 	zx = 0;
 	zy = 0;
 	cx = 0;
-	cy = 0; 
+	cy = 0;
 	iter = 0;
 	while (iter < max->data->iter && (cx + cy) <= 4)
 	{
@@ -114,14 +116,15 @@ int ft_iter_3(max_t* max)
 	return (iter);
 }
 
-void ft_colourize(void* max2)
+void	ft_colourize(void	*max2)
 {
 	union u_colour	test;
-	int32_t	x;
-	int32_t	y;
-	int32_t	z;
-	int32_t	iter;
-	max_t*	max;
+	int32_t			x;
+	int32_t			y;
+	int32_t			z;
+	int32_t			iter;
+	int				c;
+	max_t			*max;
 
 	max = max2;
 	test.rgbai = 0x00000000;
@@ -134,7 +137,6 @@ void ft_colourize(void* max2)
 	x = -1;
 	y = -1;
 	z = max->data->z;
-		
 	for (uint32_t i = 0; i < max->image->width; ++i)
 	{
 		for (uint32_t j = 0; j < max->image->height; ++j)
@@ -147,18 +149,17 @@ void ft_colourize(void* max2)
 			test.srgba.green = ((iter) / 4) % 256;
 			if (iter == max->data->iter)
 				test.rgbai = 0x000000ff;
-			uint32_t colour = test.rgbai;
-			mlx_put_pixel(max->image, i, j, colour);
+			c = test.rgbai;
+			mlx_put_pixel(max->image, i, j, c);
 		}
 	}
 }
 
-void ft_hook(void* param)
+void	ft_hook(void *param)
 {
-	max_t*	max;
+	max_t	*max;
 
 	max = param;
-
 	if (mlx_is_key_down(max->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(max->mlx);
 	if (mlx_is_key_down(max->mlx, MLX_KEY_UP))
@@ -174,7 +175,7 @@ void ft_hook(void* param)
 		if (max->data->power > 0.1)
 			max->data->power -= 0.015625;
 	}
-	if (mlx_is_key_down(max->mlx,MLX_KEY_KP_MULTIPLY))
+	if (mlx_is_key_down(max->mlx, MLX_KEY_KP_MULTIPLY))
 	{
 		if (max->data->power < 100)
 			max->data->power += 0.015625;
@@ -184,7 +185,7 @@ void ft_hook(void* param)
 		if (max->data->iter < 1024)
 			max->data->iter += 16;
 	}
-	if (mlx_is_key_down(max->mlx,MLX_KEY_KP_2))
+	if (mlx_is_key_down(max->mlx, MLX_KEY_KP_2))
 	{
 		if (max->data->iter > 16)
 			max->data->iter -= 16;
@@ -193,7 +194,7 @@ void ft_hook(void* param)
 	{
 		max->data->c--;
 	}
-	if (mlx_is_key_down(max->mlx,MLX_KEY_KP_6))
+	if (mlx_is_key_down(max->mlx, MLX_KEY_KP_6))
 	{
 		max->data->c++;
 	}
@@ -228,7 +229,7 @@ void	ft_init(data_t	*data)
 	data->temp = 0;
 }
 
-int ft_fractol(void)
+int	ft_fractol(void)
 {
 	mlx_t		*mlx;
 	data_t		datat;
@@ -240,35 +241,34 @@ int ft_fractol(void)
 	data = &datat;
 	ft_init(data);
 	max = &maxt;
-
 	image = NULL;
 	max->image = image;
 	max->data = data;
-
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!mlx)
 	{
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	max->mlx = mlx;
-	if (!(max->image = mlx_new_image(mlx, 511, 511)))
+	max->image = mlx_new_image(mlx, 511, 511);
+	if (!(max->image))
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	if (mlx_image_to_window(mlx, max->image, 0, 0) == -1)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	mlx_scroll_hook(mlx, &zoom, max);
 	max->data->zxy[0] = -2;
 	max->data->zxy[1] = -2;
 	mlx_loop_hook(mlx, ft_colourize, max);
 	mlx_loop_hook(mlx, ft_hook, max);
-
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
